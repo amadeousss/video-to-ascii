9da04ba5a559030d8ddf93050ea4e79f643ef0ed
amadeousss <amady833@gmail.com>
2022-04-29 18:47:14 +0300

    Initial commit

---
+++ frames.py
@@ -0,0 +1,60 @@
+import cv2
+import os
+from PIL import Image, ImageDraw, ImageFont
+import moviepy.video.io.ImageSequenceClip
+
+
+def video_to_frames(input_loc, output_loc):
+    vidcap = cv2.VideoCapture(input_loc)
+    success, image = vidcap.read()
+    count = 0
+    while success:
+        cv2.imwrite(f"{output_loc}/{count}.jpg", image)  # save frame as JPEG file
+        success, image = vidcap.read()
+        count += 1
+
+
+def ascii_frames_to_images(input_loc, output_loc, original_video):
+    vcap = cv2.VideoCapture(original_video)
+    width = int(vcap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Sample line of text
+    with open(f'{input_loc}/0.txt', 'r') as f:
+        sample_line = f.readlines()[0]
+
+    # Compute font size
+    fontsize = 1
+    img_fraction = 1
+    font = ImageFont.truetype("cour.ttf", fontsize)
+    image = Image.new('RGB', (width, height), color='black')
+    draw = ImageDraw.Draw(image)
+    while draw.textlength(sample_line[0], font=font) * len(sample_line) < img_fraction * width:
+        print(f"Fontsize: {fontsize}")
+        fontsize += 1
+        font = ImageFont.truetype("cour.ttf", fontsize)
+
+    # Put text on black bg and save frames
+    for index, filename in enumerate(os.listdir(input_loc)):
+        with open(f'{input_loc}/{index}.txt', 'r') as f:
+            content = f.read()
+            image = Image.new('RGB', (width, height), color='black')
+            ImageDraw.Draw(
+                image
+            ).text(
+                (0, 0),
+                content,
+                (255, 255, 255),
+                font=font
+            )
+            image.save(f"{output_loc}/{index}.png")
+
+
+def frames_to_video(input_loc, output_name, original_video):
+    cap = cv2.VideoCapture(original_video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    image_files = []
+    for index, filename in enumerate(os.listdir(input_loc)):
+        image_files.append(os.path.join(input_loc, f"{index}.png"))
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+    clip.write_videofile(output_name)
---
+++ main.py
@@ -0,0 +1,65 @@
+from PIL import Image
+from frames import video_to_frames, ascii_frames_to_images, frames_to_video
+import os
+
+# CHARS = " `^\",;li~+?[}{1)(\\/tzXZ*#&8%@$"
+CHARS = " .:-=+*#%@"
+
+
+def correct_round(val):
+    if val - int(val) == 0.5:
+        return int(val) + 1
+    else:
+        return round(val)
+
+
+def map_to_char(val):
+    return CHARS[int(val / 255 * (len(CHARS) - 1))]
+
+
+def image_to_ascii(image_path):
+    im = Image.open(image_path)
+    width, height = im.size
+    new_width = 40
+    new_height = round(new_width * height / width)
+    im = im.resize((new_width, new_height))
+    width, height = im.size
+    grid = list(im.getdata())
+    grid = [grid[i:i + im.width] for i in range(0, len(grid), im.width)]
+    content = ""
+    for y in range(height):
+        for x in range(width):
+            grid[y][x] = map_to_char(sum(grid[y][x]) / 3)
+    for y in range(height):
+        for x in range(width):
+            content += grid[y][x] * 2
+        content += "\n"
+    # print(content)
+    return content
+
+
+def video_to_ascii(video_path):
+    video_to_frames(video_path, 'frames/')
+    for index, filename in enumerate(os.listdir('frames/')):
+        content = image_to_ascii(os.path.join('frames/', f"{index}.jpg"))
+        with open(f'frames_ascii_txt/{index}.txt', 'w') as f:
+            f.write(content)
+
+
+def main():
+    video_to_ascii('video.MP4')
+    ascii_frames_to_images('frames_ascii_txt', 'frames_ascii_img', 'video.mp4')
+    frames_to_video('frames_ascii_img', 'video_ascii.mp4', 'video.mp4')
+
+    for f in os.listdir('frames_ascii_txt/'):
+        os.remove(os.path.join('frames_ascii_txt/', f))
+
+    for f in os.listdir('frames/'):
+        os.remove(os.path.join('frames/', f))
+
+    for f in os.listdir('frames_ascii_img/'):
+        os.remove(os.path.join('frames_ascii_img/', f))
+
+
+if __name__ == '__main__':
+    main()
